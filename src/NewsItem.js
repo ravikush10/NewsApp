b9c2daaa89a2903fb43f10a796dbd91782ac5c57
Devendra Kushwaha <ravik6440@gmail.com>
2023-05-06 14:49:10 +0530

    function based component

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
+const NewsItem = (props) => {
 
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-        return (
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
 
-            <div className="my-3">
-                <div className="card" >
-                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}>
-                        {source}
-                        <span className="visually-hidden">unread messages</span>
-                    </span>
-                    <div className="image">
-                        <div id="zoomOut">
-                            <a rel="noreferrer" href={newsUrl} target="_blank">
-                                <img src={imageUrl ? imageUrl : "https://static.vecteezy.com/system/resources/thumbnails/004/216/831/original/3d-world-news-background-loop-free-video.jpg"} className="card-img-top" alt="..." />
-                            </a>
-                        </div>
-                    </div>
-                    <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
-                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
-                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+        <div className="my-3">
+            <div className="card" >
+                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}>
+                    {source}
+                    <span className="visually-hidden">unread messages</span>
+                </span>
+                <div className="image">
+                    <div id="zoomOut">
+                        <a rel="noreferrer" href={newsUrl} target="_blank">
+                            <img src={imageUrl ? imageUrl : "https://static.vecteezy.com/system/resources/thumbnails/004/216/831/original/3d-world-news-background-loop-free-video.jpg"} className="card-img-top" alt="..." />
+                        </a>
                     </div>
                 </div>
+                <div className="card-body">
+                    <h5 className="card-title">{title}...</h5>
+                    <p className="card-text">{description}...</p>
+                    <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+                    <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                </div>
             </div>
+        </div>
 
-        )
-    }
+    )
+
 }
 
 export default NewsItem
